3612bde72d879774096a52dcb88ae57b3b383039
Denis Accardo <justdenys@gmail.com>
2023-08-17 16:40:48 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,37 @@ const MAX_VALUE = 999;
 const MIN_VALUE = 0;
 
 
+window.onload = function () {   //THIS FUNCTION LOADS THE EVENTS ONLY WHEN THE PAGE STRUCTURE IS FULLY LOADED
+
+    const body = document.body;
+    const main = createElements('main', body);
+    const container = createElements('div', main, null, null, ['container']);
+    const buttonUp = createElements('button', container, null, null, null, 'buttonUp');
+    createElements('img', buttonUp, '/assets/images/arrow-up.svg', 'arrow up', ['arrow-up', 'increase'], null, null, 'increase');
+    const counterAndReset = createElements('div', container, null, null, ['counter', 'reset'], null, null, 'reset');
+    createElements('p', counterAndReset, null, null, ['countLabel'], null, '0');
+    const buttonDown = createElements('button', container, null, null, null, 'buttonDown');
+    createElements('img', buttonDown, '/assets/images/arrow-up.svg', 'arrow down', ['arrow-down', 'decrease'], null, null, 'decrease');
+
+
+    countLabel = document.querySelector('.countLabel'); // FOR COUNTER VALUE MODIFICATION
+
+
+    // BUTTONS EVENT DELEGATION
+    container.addEventListener('click', (event) => {
+        const action = event.target.dataset.action;
+        if (action === 'decrease') {                    // DECREASE THE COUNTER VALUE
+            updateLabelCounter(actualValue - 1)
+
+        } else if (action === 'increase') {          // INCREASE THE COUNTER VALUE
+            updateLabelCounter(actualValue + 1);
+
+        } else if (action === 'reset') {            // RESET THE COUNTER VALUE
+            updateLabelCounter(0)
+        }
+    });
+};
+
 //THIS FUNCTION CREATES THE PAGE STRUCTURE
 function createElements(eTag, eParent, eSrc = '', eAlt = '', eClass = [], eId = '', eContent = '', eDataAction = '') {
 
@@ -57,33 +88,3 @@ function resetLabelCounter(limitValue) {    //RESETS THE COUNTER VALUE IF UNDER 
 };
 
 
-window.onload = function () {   //THIS FUNCTION LOADS THE EVENTS ONLY WHEN THE PAGE STRUCTURE IS FULLY LOADED
-
-    const body = document.body;
-    const main = createElements('main', body);
-    const container = createElements('div', main, null, null, ['container']);
-    const buttonUp = createElements('button', container, null, null, null, 'buttonUp');
-    createElements('img', buttonUp, '/assets/images/arrow-up.svg', 'arrow up', ['arrow-up', 'increase'], null, null, 'increase');
-    const counterAndReset = createElements('div', container, null, null, ['counter', 'reset'], null, null, 'reset');
-    createElements('p', counterAndReset, null, null, ['countLabel'], null, '0');
-    const buttonDown = createElements('button', container, null, null, null, 'buttonDown');
-    createElements('img', buttonDown, '/assets/images/arrow-up.svg', 'arrow down', ['arrow-down', 'decrease'], null, null, 'decrease');
-
-
-    countLabel = document.querySelector('.countLabel'); // FOR COUNTER VALUE MODIFICATION
-
-
-    // BUTTONS EVENT DELEGATION
-    container.addEventListener('click', (event) => {
-        const action = event.target.dataset.action;
-        if (action === 'decrease') {                    // DECREASE THE COUNTER VALUE
-            updateLabelCounter(actualValue - 1)
-
-        } else if (action === 'increase') {          // INCREASE THE COUNTER VALUE
-            updateLabelCounter(actualValue + 1);
-
-        } else if (action === 'reset') {            // RESET THE COUNTER VALUE
-            updateLabelCounter(0)
-        }
-    });
-};

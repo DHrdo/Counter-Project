c88e4fb731f70fb7d10f57dbe5650e69dca0b3d6
Denis Accardo <justdenys@gmail.com>
2023-08-17 16:36:59 +0200

    Modified createElements() function
    Modified createElements() function and added constants instead of querySelector().
    Optimization of js scripts

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,15 +3,6 @@ let actualValue = 0;
 const MAX_VALUE = 999;
 const MIN_VALUE = 0;
 
-createElements('main', 'body');
-createElements('div', document.querySelector('main'), null, null, ['container']);
-createElements('button', document.querySelector('.container'), null, null, null, 'buttonUp');
-createElements('img', document.querySelector('#buttonUp'), '/assets/images/arrow-up.svg', 'arrow up', ['arrow-up', 'increase'], null, null, 'increase');
-createElements('div', document.querySelector('.container'), null, null, ['counter', 'reset']);
-createElements('p', document.querySelector('.counter'), null, null, ['countLabel'], null, '0');
-
-createElements('button', document.querySelector('.container'), null, null, null, 'buttonDown');
-createElements('img', document.querySelector('#buttonDown'), '/assets/images/arrow-up.svg', 'arrow down', ['arrow-down', 'decrease'], null, null, 'decrease');
 
 //THIS FUNCTION CREATES THE PAGE STRUCTURE
 function createElements(eTag, eParent, eSrc = '', eAlt = '', eClass = [], eId = '', eContent = '', eDataAction = '') {
@@ -46,6 +37,8 @@ function createElements(eTag, eParent, eSrc = '', eAlt = '', eClass = [], eId = 
     if (eDataAction) {
         element.setAttribute('data-action', eDataAction);
     }
+
+    return element; //returns eTag
 };
 
 function updateLabelCounter(newCount) {     //UPDATES AND SHOWS THE COUNTER VALUE
@@ -66,14 +59,22 @@ function resetLabelCounter(limitValue) {    //RESETS THE COUNTER VALUE IF UNDER 
 
 window.onload = function () {   //THIS FUNCTION LOADS THE EVENTS ONLY WHEN THE PAGE STRUCTURE IS FULLY LOADED
 
-    const decrease = document.querySelector('.decrease');
-    const increase = document.querySelector('.increase');
-    countLabel = document.querySelector('.countLabel');
-    const reset = document.querySelector('.reset');
+    const body = document.body;
+    const main = createElements('main', body);
+    const container = createElements('div', main, null, null, ['container']);
+    const buttonUp = createElements('button', container, null, null, null, 'buttonUp');
+    createElements('img', buttonUp, '/assets/images/arrow-up.svg', 'arrow up', ['arrow-up', 'increase'], null, null, 'increase');
+    const counterAndReset = createElements('div', container, null, null, ['counter', 'reset'], null, null, 'reset');
+    createElements('p', counterAndReset, null, null, ['countLabel'], null, '0');
+    
+    const buttonDown = createElements('button', container, null, null, null, 'buttonDown');
+    createElements('img', buttonDown, '/assets/images/arrow-up.svg', 'arrow down', ['arrow-down', 'decrease'], null, null, 'decrease');
 
 
-    // EVENT DELEGATION *( FEEDBACK FIX )*
-    const container = document.querySelector('.container');
+    countLabel = document.querySelector('.countLabel'); // FOR COUNTER VALUE MODIFICATION
+
+
+    // BUTTONS EVENT DELEGATION
     container.addEventListener('click', (event) => {
         const action = event.target.dataset.action;
         if (action === 'decrease') {                    // DECREASE THE COUNTER VALUE
@@ -82,7 +83,7 @@ window.onload = function () {   //THIS FUNCTION LOADS THE EVENTS ONLY WHEN THE P
         } else if (action === 'increase') {          // INCREASE THE COUNTER VALUE
             updateLabelCounter(actualValue + 1);
 
-        } else if (action === 'reset') {          // RESET THE COUNTER VALUE
+        } else if (action === 'reset') {            // RESET THE COUNTER VALUE
             updateLabelCounter(0)
         }
     });
